261187744d7a59dabaa287f139ab3c60a34529d5
Mauricio Hernandez <mauriciohernandez@MacBook-Pro.local>
2021-02-03 19:05:38 -0500

    unnecesary code

--- events-handler/app.py
+++ events-handler/app.py
@@ -71,16 +71,6 @@ users = [
 ]
 
 
-def find_user(lista, email):
-    return lista['email'] == email
-    # lista_filtrada = []
-    # for i in lista :
-    #     print(i)
-    #     if i['email'] == email :
-    #         lista_filtrada.append(i)
-    # return lista_filtrada
-
-
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -185,11 +175,7 @@ def event(id):
 @app.route('/create-event', methods=['GET', 'POST'])
 @cross_origin()
 def create_event():
-    # SELECT events.id AS events_id
-    # FROM events ORDER BY -events.id
-    #  LIMIT %(param_1)s
-    # lastevent = Events.query.with_entities(Events.id).order_by(-Events.id).limit(1)
-    # print(lastevent.scalar())
+
     new_name = request.json['name']
     new_category = request.json['category']
     new_place = request.json['place']
@@ -221,18 +207,6 @@ def edit_event(event_id):
             Events.address: new_address
         }
     )
-    # if column == "name":
-    #     Events.query.filter_by(id=event_id).update({Events.name: new_value})
-    # else:
-    #     if column == "category":
-    #         Events.query.filter_by(id=event_id).update({Events.category: new_value})
-    #     else:
-    #         if column == "place":
-    #             Events.query.filter_by(id=event_id).update({Events.place: new_value})
-    #         else:
-    #             if column == "address":
-    #                 Events.query.filter_by(id=event_id).update({Events.address: new_value})
-
     db.session.commit()
     events = Events.query.filter_by(user_email=user_email)
     return events_schema.jsonify(events)
@@ -250,41 +224,6 @@ def delete_event(event_id):
     return events_schema.jsonify(events)
 
 
-@app.route('/')
-def inicio():
-    if 'username' in session:
-        x = list(filter(lambda users_list: find_user(users_list, session["username"]), users))
-        return jsonify(x)
-    return 'usuario no loggeado'
-    # app.logger.debug('mensaje debug')
-    app.logger.info(f'entramos al path {request.path}')
-    # app.logger.warn('mensaje warn')
-    # app.logger.error('mensaje error')
-
-
-@app.route('/login/<email>', methods=['GET', 'POST'])
-def login(email):
-    if request.method == 'POST':
-        session['username'] = email
-        return redirect(url_for('inicio'))
-    return render_template('login.html')
-
-
-@app.route('/signup/<email>/<pwd>', methods=['GET', 'POST'])
-@cross_origin()
-def signup(email, pwd):
-    global users
-    new_user = {"email": email, "pwd": pwd, "events": []}
-    users.append(new_user)
-    return jsonify(users)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('username')
-    return redirect(url_for('inicio'))
-
-
 @app.route('/salir')
 def salir():
     return abort(404)
@@ -293,65 +232,3 @@ def salir():
 @app.errorhandler(404)
 def pagina_no_encontrada(error):
     return render_template('error404.html', error=error), 404
-
-
-@app.route('/list-users', methods=['GET', 'POST'])
-def list_users():
-    global users
-    return jsonify(users)
-
-
-@app.route('/user-details/<email>', methods=['GET', 'POST'])
-@cross_origin()
-def user_details(email):
-    global users
-    # valores = {'email': email, 'methodo_http': request.method}
-    # users.append(email)
-    # x = filtro_nombre(users, email)
-    x = list(filter(lambda users_list: find_user(users_list, email), users))
-    return jsonify(x)
-
-
-def validate_pwd(user, email, pwd):
-    return user['pwd'] == pwd and user['email'] == email
-
-
-@app.route('/validate-pwd/<email>/<pwd>', methods=['GET', 'POST'])
-@cross_origin()
-def validate_user(email, pwd):
-    global users
-    # valores = {'email': email, 'methodo_http': request.method}
-    # users.append(email)
-    # x = filtro_nombre(users, email)
-    x = list(filter(lambda users_list: validate_pwd(users_list, email, pwd), users))
-    return jsonify(x)
-
-
-def delete_user(users_list, email):
-    return users_list['email'] != email
-
-
-@app.route('/delete-user/<email>', methods=['GET', 'POST'])
-def delete_users(email):
-    global users
-    # valores = {'email': email, 'methodo_http': request.method}
-    # users.append(email)
-    # x = filtro_nombre(users, email)
-    user = list(filter(lambda users_list: delete_user(users_list, email), users))
-    users = user
-    return jsonify(users)
-
-
-@app.route('/update-pwd/<email>/<pwd>', methods=['GET', 'POST'])
-def update_pwd(email, pwd):
-    global users
-    filtered_users = []
-    for i in users:
-        print(i)
-        if i['email'] == email:
-            filtered_users.append({'email': email, 'pwd': pwd})
-        else:
-            filtered_users.append(i)
-    users = filtered_users
-    user = list(filter(lambda users_list: find_user(users_list, email), users))
-    return jsonify(user)
